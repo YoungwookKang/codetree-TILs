80d8b74f9cce373c8ac228a6e91f69171fc7e2a1
YoungwookKang <125630695+YoungwookKang@users.noreply.github.com>
2024-10-10 14:16:48 +0900

    Title: 외주 수익 최대화하기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241010/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -0,0 +1,17 @@
+n = int(input())
+days = [list(map(int, input().split())) for _ in range(n)]
+max_value = 0
+
+def backtracking(curr, n, days, current_value):
+    global max_value
+    if curr >= n:
+        max_value = max(current_value, max_value)
+        return
+    
+    duration, profit = days[curr][0], days[curr][1]
+    backtracking(curr+duration, n, days, current_value + profit)
+    backtracking(curr+1, n, days, current_value)
+
+backtracking(0,n,days,0)
+
+print(max_value)
